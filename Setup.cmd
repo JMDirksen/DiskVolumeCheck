c38ca05ac041abba5a53b6a0039c7a9c6975158d
JMDirksen <jeftadirksen@gmail.com>
2023-08-06 17:26:09 +0200

    Elevated/system permissions not required

--- Setup.cmd
+++ Setup.cmd
@@ -1,6 +1,4 @@
 @echo off
-schtasks /create /tn "DiskVolumeCheck" /tr "powershell.exe -f %~dp0DiskVolumeCheck.ps1" /ru system /sc minute /mo 15 /st 00:00
+schtasks /create /tn "DiskVolumeCheck" /tr "powershell.exe -f %~dp0DiskVolumeCheck.ps1" /sc minute /mo 15 /st 00:00
 echo.
-echo.
-if %ERRORLEVEL%==1 echo Make sure to run this setup with elevated permissions (Run as administrator).
 pause
